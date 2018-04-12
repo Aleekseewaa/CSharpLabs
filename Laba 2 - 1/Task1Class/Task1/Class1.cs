c5e2fb14bad6d8f5180c261092ac8889cf04ef43
Aleekseewaa <aleekseewaa@yandex.ru>
2018-04-12 16:34:47 +0300

    Add files via upload

---
+++ Laba 2 - 1/Task1Class/Task1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class BitwiseOperations
+    {
+        public static void LeftRight(int n, int k)
+        {
+            int resultLk = n << k;
+            int resultLn = k << n;
+            int resultRk = n >> k;
+            int resultRn = k >> n;
+            Console.WriteLine("Свиг влево на {0} шагов = {1}", k, resultLk);
+            Console.WriteLine("Сдвиг влево на {0} шагов = {1}",n,resultLn);
+            Console.WriteLine("Сдвиг вправо на {0} шагов = {1}", n, resultRk);
+            Console.WriteLine("Сдвиг вправо на {0} шагов = {1}", n, resultRn);
+        }
+
+        public static void BwO(int x, int y)
+        {
+            int r = x & y;
+            int r1 = x ^ y;
+            int r2 = x | y;
+            Console.WriteLine(r + " " + r1 + " " + r2);
+        }
+        
+        public static void Assignment(int k)
+        {
+            int result = 3;
+            Console.WriteLine("Операция <<= {0}", result <<= k); //сдигает res на k влево
+            Console.WriteLine("Операция >>= {0}", result >>= k);//сдигает res на k влево
+            Console.WriteLine("Операция &= {0}", result &= k);
+            Console.WriteLine("Операция ^= {0}", result ^= k);
+            Console.WriteLine("Операция |= {0}", result |= k);
+        }
+    }
+}
---
+++ Laba 2 - 1/TestTask1/TestDll/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace TestDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            for (; ;)
+            {
+                Console.WriteLine("Выберите библиотеку для теста:");
+                Console.WriteLine("1: Сдвиг влево, Сдвиг вправо");
+                Console.WriteLine("=====================================");
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ваш выбор = {0}. Запускаем тест", n);
+                Console.WriteLine();
+                switch (n)
+                {
+                    case 1:
+                        BitwiseOperations.LeftRight(4, 1);
+                        BitwiseOperations.Assignment(2);
+                        break;
+                }
+            }
+        }
+    }
+}
